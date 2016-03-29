666d7ee05c2431168e6e4d7b591d08f042df10dd
xwillmadeit <xiesen01@outlook.com>
2016-03-30 07:28:50 +0800

    demo

--- requireDemo/js/main.js
+++ requireDemo/js/main.js
@@ -1,10 +1,10 @@
-requirejs.config({
+require.config({
 	paths: {
 		'jquery': '../bower_components/jquery/dist/jquery.min'
 	}
 	//如果 jquery 在 main.js 文件同级目录且叫 jquery.js 则不需要指定 path 和别名
 });
-requirejs(['jquery','validate'],function($,validate){
+require(['jquery','validate'],function($,validate){
 	$('body').css('background-color','red');
 	$('#getVal').click(function(){
 		console.log(validate.getVal('input'));
